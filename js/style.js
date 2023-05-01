2cd4505bd32f28148da0e415f46cf73a8a56a8d1
Zotixxx <oneman8368@gmail.com>
2023-05-01 09:56:09 -0700

    uploded

---
+++ js/style.js
@@ -0,0 +1,47 @@
+
+
+/*==================== MUDAR A COR DO HEADER ====================*/
+function scrollHeader(){
+    const scrollHeader = document.getElementById('header')
+    if(this.scrollY >= 100) scrollHeader.classList.add('scroll-header'); else scrollHeader.classList.remove('scroll-header')
+
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: { 
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
+  function redirectToInsta() {
+    window.location.href = "https://www.instagram.com/your_username/";
+  }
+  
+  
